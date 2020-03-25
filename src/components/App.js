7f247e30b88e2c36ff8946c6e0c21834ec7fdeb9
Jialin Shan <chansukeizumi@gmail.com>
2020-03-24 21:59:28 -0400

    ugly data gotten

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class App extends Component {
     state = {
         lat: 0,
-        lon: 0
+        lon: 0,
+        data: null
     }
 
     componentDidMount() {
@@ -34,18 +35,43 @@ class App extends Component {
         let fetchData = await fetch(`https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lon}`)
         let json = await fetchData.json();
 
+        this.setState({
+            location: `${json.name} ${json.sys.country}`,
+            data: json
+        })      
+
         console.log(json);
-        
     }
 
     render() {
+        let json = this.state.data;
+        let info = null;
+        if( json != null ) {
+            console.log('TRUE')
+            let weather = json.weather[0];
+            let temp = json.main.temp;
+            let weatherIcon = <img src={weather.icon} ></img>;
+            info = <div>
+                {weather.main} 
+                <br/>
+                {weather.description}
+                <br/>
+                {weatherIcon} 
+                <br/>
+                {temp} celcius
+            </div>
+        }
+
+        console.log(info, this.state);
 
         return (
             <div>
                 {/* <button onClick={this.getLocation}>Location for Weather!</button> */}
+                <p> Location: {this.state.location}</p>
                 <p> Your current coordinates are... </p>
                 <p> Latitude: {this.state.lat} </p>
                 <p> Longitude: {this.state.lon}</p>
+                {info}
             </div>
         )
     }
